a509bd8b456b66191ce20ee6d5693c0fd0959757
Ale Enjandro <ciclost@pc-33>
2021-11-23 19:35:25 +0100

    Arreglada una unidad de bug en procesar_pedido

--- Proyecto/procesar_pedido.php
+++ Proyecto/procesar_pedido.php
@@ -1,6 +1,6 @@
 <?php
 require 'correo.php';
-require 'sesiiones.php';
+require 'sesiones.php';
 require_once 'bd-php';
 comprobar_sesion();
 ?>
@@ -14,7 +14,7 @@ comprobar_sesion();
         <?php
         require 'cabecera.php';
         $resul=insertar_pedido($_SESSION['carrito'],$_SESSION['usuario']);
-        if ($resul=== false){
+        if ($resul===false){
             echo "No se ha podido realizar el pedido<br>";
         } else {
             echo "Pedido realizado con éxito. Se enviará un correo de confirmación.";
